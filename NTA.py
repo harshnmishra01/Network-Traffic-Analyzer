83ad17e078fc1480a6059ad454d4db1983eea5af
Harsh <97902881+harshnmishra01@users.noreply.github.com>
2023-10-13 12:33:49 +0530

    Add files via upload

---
+++ NTA.py
@@ -0,0 +1,16 @@
+import scapy.all as scapy
+
+def sniff_packets(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_packet)
+
+def process_packet(packet):
+    if packet.haslayer(scapy.IP):
+        source_ip = packet[scapy.IP].src
+        destination_ip = packet[scapy.IP].dst
+        protocol = packet[scapy.IP].proto
+
+        print(f"Source IP: {source_ip}, Destination IP: {destination_ip}, Protocol: {protocol}")
+
+if __name__ == "__main__":
+    interface = "eth0"
+    sniff_packets(interface)
